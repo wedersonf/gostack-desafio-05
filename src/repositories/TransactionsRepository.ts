dc40a9bb48e366b3599ce3ee688ed131051c9229
wedersonf <wedymf@hotmail.com>
2021-01-11 19:02:28 -0300

    funcionalidades do desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -8,10 +8,38 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
+
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce(
+      (accumulator, currentValue) => {
+        if (currentValue.type === 'income') {
+          accumulator.income += Number(currentValue.value);
+        } else if (currentValue.type === 'outcome') {
+          accumulator.outcome += Number(currentValue.value);
+        }
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
